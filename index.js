63eeac0126c070f26175b806ba6c81c955035e89
Alejandro Velez <velez.alejandro4@gmail.com>
2024-02-16 19:53:18 -0500

    completed chatbot feature and allowed for chatbot to recall coversation history

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 import openai from "./config/open-ai.js";
 import readlineSync from "readline-sync";
-import colors from 'colors';
+import colors from "colors";
 
 async function main() {
-    console.log(colors.bold.green('Welcome to Alejandros ChatBot'));
-    console.log(colors.bold.green('You can start chatting now'));
+  console.log(colors.bold.green("Welcome to Alejandros ChatBot"));
+  console.log(colors.bold.green("You can start chatting now"));
 
-    while (true) {
-        const userInput = readlineSync.question(colors.yellow('You: '));
+  const chatHistory = [];
 
-        try {
-            //Call the API with user input
+  while (true) {
+    const userInput = readlineSync.question(colors.yellow("You: "));
 
-            if (userInput.toLowerCase() === 'exit') {
-                return;
-            }
-        } catch (error) {
-            console.error(colors.red(error));
-        }
+    try {
+      //consruct messages by iterating over the history
+      const messages = chatHistory.map(([role, content]) => ({
+        role,
+        content,
+      }));
+
+      //add latest user input
+      messages.push({ role: "user", content: userInput });
+
+      const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: messages,
+      });
+
+      //get completion text/content
+
+      const completionText = chatCompletion.choices[0].message.content;
+
+      if (userInput.toLowerCase() === "exit") {
+        console.log(colors.green("Bot: ") + completionText);
+        return;
+      }
+      console.log(colors.green("Bot: ") + completionText);
+
+      //update history with user input and assistant response
+      chatHistory.push(["user", userInput]);
+      chatHistory.push(["assistant", completionText]);
+    } catch (error) {
+      console.error(colors.red(error));
     }
+  }
 }
 
 main();
